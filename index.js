5fca812b227d15f999dc600429547d256c66151c
menty44 <menty44@gmail.com>
2021-02-18 08:44:35 +0300

    chnaged logic to the main entry file

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-const custom = require('./service');
+const myCountries = require('./countries');
 
-function ByPhoneNumber(phone) {
-    custom.getByPhoneNumber(phone);
+let countries = myCountries.data;
+
+function getByPhoneNumber(phone) {
+
+    for (let i = 0; i < countries.length; i++) {
+        if (phone.startsWith(countries[i].phonecode)) {
+            return countries[i];
+        }
+    }
 }
 
-function ByCountryCode(countryCode) {
-    custom.getByCountryCode(countryCode);
+function getByCountryCode(countryCode) {
+
+    for (let i = 0; i < countries.length; i++) {
+        if (countryCode.toUpperCase().startsWith(countries[i].iso)) {
+            return countries[i];
+        }
+    }
 }
 
-function ByCountryName(CountryName) {
-    custom.getByCountryName(CountryName);
+function getByCountryName(CountryName) {
+
+    for (let i = 0; i < countries.length; i++) {
+        if (CountryName.toUpperCase().startsWith(countries[i].name)) {
+            return countries[i];
+        }
+    }
 }
 
 
-//Exports
-module.exports.ByPhoneNumber= ByPhoneNumber;
-module.exports.ByCountryCode= ByCountryCode;
-module.exports.ByCountryName= ByCountryName;
+module.exports = {
+    getByPhoneNumber: getByPhoneNumber,
+    getByCountryCode: getByCountryCode,
+    getByCountryName: getByCountryName,
+};
